df635a1c9508abbdb6dbbc19ee79d759b93a3cfd
Dominik Ocsofszki <dominikocsofszki@gmail.com>
2025-05-25 18:05:45 +0200

    rename

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,5 @@
 
-docker-buildx build -f Dockerfile_swe -t my-temp-image . || exit 1
+docker-buildx build -f Dockerfile -t my-temp-image . || exit 1
 docker run -d --name my-temp-container my-temp-image || exit 1
 docker exec -it my-temp-container /bin/bash
 
